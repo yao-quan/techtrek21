9a07a8062e83cb2024630fdc5df3655f5686917a
lycena <lycena@outlook.com>
2021-11-11 11:44:21 +0800

    Backend initial setup

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,40 @@
-// import express
-// set up routes
-// listen on port
+import express from "express";
+import mongoose from "mongoose";
+import Cors from "cors";
+import { User, Project, Category, Expense } from "./models/index.js";
 
-const express = require('express')
-const mongoose = require('mongoose')
-const home = require('./routes/home')
+// App Config
+const app = express();
+const port = process.env.PORT || 8001; // Using 8001 because rarely used
 
-const PORT = 8080
-const app = express()
+// DB Config
+const connection_url =
+  "mongodb+srv://techtrek21:techtrek21@techtrek-21.hjfbd.mongodb.net/dbs-techtrek-21?retryWrites=true&w=majority";
+mongoose.connect(connection_url);
 
-const user = `yao_quan`
-const password = `4FXYF!m8AhA`
-const db = `react-test`
+// Middleware
+app.use(express.json());
+app.use(Cors()); // Enables CORS
 
-mongoose.connect(`mongodb+srv://${user}:${password}@react-test.cxmrc.mongodb.net/${db}?retryWrites=true&w=majority`)
-const connection = mongoose.connection
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+// API Listener
+app.listen(port, () => {
+  console.log("Listening on localhost:" + port);
 });
 
-app.use('/', home)
-  
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-})
+// API Endpoint
+app.get("/", (req, res) => {
+  res.status(200).send("Endpoint connected");
+});
+
+// const user = `yao_quan`
+// const password = `4FXYF!m8AhA`
+// const db = `react-test`
+
+// mongoose.connect(`mongodb+srv://${user}:${password}@react-test.cxmrc.mongodb.net/${db}?retryWrites=true&w=majority`)
+// const connection = mongoose.connection
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
