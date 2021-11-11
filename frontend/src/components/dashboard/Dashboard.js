f2cdc71ba1aabcbaf0b12758b74c7187466ac065
xiaochaorenn <54619857+xiaochaorenn@users.noreply.github.com>
2021-11-11 14:45:16 +0800

    Add New Project Modal
    UI done

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,9 +1,35 @@
 import classes from "./Dashboard.module.css";
+import React, { useState } from "react";
+import "./style.css";
+import "antd/dist/antd.css";
+import { Form, Radio, Modal, Input, Button, Checkbox } from "antd";
 
 function Dashboard() {
+  const [visible, setVisible] = useState(false);
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    /* this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000); */
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <div>
-      <h1>Project Budget Management</h1>
+      <h1 className={classes.mainPageHeading}>Project Budget Management</h1>
 
       <div className={classes.dashboardHead}>
         <div className="graph-1">12 Managing Project</div>
@@ -12,7 +38,12 @@ function Dashboard() {
 
       <div class="dashboard-body">
         <div className={classes.projectContainer}>
-          <div className={classes.projectTitle}>Projects</div>
+          <div className={classes.projects}>
+            Projects
+            <Button type="primary" onClick={showModal}>
+              Add
+            </Button>
+          </div>
           <div className={classes.listGroup}>
             <div
               className={
@@ -31,6 +62,77 @@ function Dashboard() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="Model">
+        <Modal
+          visible={visible}
+          title="Add New Project Budget"
+          onOk={handleOk}
+          onCancel={handleCancel}
+          footer={[
+            <Button key="back" onClick={handleCancel}>
+              Return
+            </Button>,
+            <Button
+              form="myForm"
+              htmlType="submit"
+              key="submit"
+              type="primary"
+              //loading={loading}
+              onClick={handleOk}
+            >
+              Submit
+            </Button>,
+          ]}
+        >
+          <Form
+            id="myForm"
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Project Name"
+              name="projectName"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the Project's Name",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Budget"
+              name="projectBudget"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the Project's Budget",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Description"
+              name="projectDesc"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the Project's Descriptions",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Form>
+        </Modal>
       </div>
     </div>
   );
