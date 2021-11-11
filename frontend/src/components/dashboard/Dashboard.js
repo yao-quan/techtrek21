148c224d2863b2eefc16eb78535730a3005e30e0
xiaochaorenn <54619857+xiaochaorenn@users.noreply.github.com>
2021-11-11 15:31:18 +0800

    Dashboard Dynamic Display

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -10,6 +10,23 @@ function Dashboard() {
     setVisible(true);
   };
 
+  const state = {
+    projectItems: [
+      {
+        id: 0,
+        title: "Project 1",
+        description: "This is a Project",
+        budget: "2,000",
+      },
+      {
+        id: 1,
+        title: "Project 2",
+        description: "This is a Project",
+        budget: "10,000",
+      },
+    ],
+  };
+
   const handleOk = () => {
     /* this.setState({ loading: true });
     setTimeout(() => {
@@ -27,114 +44,124 @@ function Dashboard() {
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+
   return (
-    <div>
-      <h1 className={classes.mainPageHeading}>Project Budget Management</h1>
-
-      <div className={classes.dashboardHead}>
-        <div className="graph-1">12 Managing Project</div>
-        <div className="graph-2">1,200,000 total</div>
-      </div>
-
-      <div class="dashboard-body">
-        <div className={classes.projectContainer}>
-          <div className={classes.projects}>
-            Projects
-            <Button type="primary" onClick={showModal}>
-              Add
-            </Button>
+    <React.Fragment>
+      <div>
+        <h1 className={classes.mainPageHeading}>Project Budget Management</h1>
+        <div className={classes.dashboard}>
+          <div className={classes.dashboardHead}>
+            <div className={classes.totalProjects}>Managed Projects: 12</div>
+            <div className={classes.totalBudget}>Total Budget:1,200,000</div>
           </div>
-          <div className={classes.listGroup}>
-            <div
-              className={
-                (classes.listGroupItemAction,
-                classes.listGroupItem,
-                classes.listGroupItem2)
-              }
-            >
-              <div className={classes.projectTitle}>
-                RTF
-                <div className={classes.projectDescription}>
-                  Realtime Face Recognition.
-                </div>
+
+          <div className={classes.dashboardBody}>
+            <div className={classes.projectContainer}>
+              <div className={classes.projects}>
+                Projects
+                <Button type="primary" onClick={showModal}>
+                  Add
+                </Button>
               </div>
-              <div className={classes.projectBudget}>$12,000</div>
+              {state.projectItems.map((projectList) => (
+                <>
+                  <div className={classes.listGroup}>
+                    <div
+                      className={
+                        (classes.listGroupItemAction,
+                        classes.listGroupItem,
+                        classes.listGroupItem2)
+                      }
+                    >
+                      <div className={classes.projectTitle}>
+                        {projectList.title}
+                        <div className={classes.projectDescription}>
+                          {projectList.description}
+                        </div>
+                      </div>
+                      <div className={classes.projectBudget}>
+                        {projectList.budget}
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ))}
             </div>
           </div>
         </div>
-      </div>
-      <div className="Model">
-        <Modal
-          visible={visible}
-          title="Add New Project Budget"
-          onOk={handleOk}
-          onCancel={handleCancel}
-          footer={[
-            <Button key="back" onClick={handleCancel}>
-              Return
-            </Button>,
-            <Button
-              form="myForm"
-              htmlType="submit"
-              key="submit"
-              type="primary"
-              //loading={loading}
-              onClick={handleOk}
-            >
-              Submit
-            </Button>,
-          ]}
-        >
-          <Form
-            id="myForm"
-            name="basic"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
+        <div className="Model">
+          <Modal
+            visible={visible}
+            title="Add New Project Budget"
+            onOk={handleOk}
+            onCancel={handleCancel}
+            footer={[
+              <Button key="back" onClick={handleCancel}>
+                Return
+              </Button>,
+              <Button
+                form="myForm"
+                htmlType="submit"
+                key="submit"
+                type="primary"
+                //loading={loading}
+                onClick={handleOk}
+              >
+                Submit
+              </Button>,
+            ]}
           >
-            <Form.Item
-              label="Project Name"
-              name="projectName"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input the Project's Name",
-                },
-              ]}
+            <Form
+              id="myForm"
+              name="basic"
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
             >
-              <Input />
-            </Form.Item>
+              <Form.Item
+                label="Project Name"
+                name="projectName"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input the Project's Name",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-            <Form.Item
-              label="Budget"
-              name="projectBudget"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input the Project's Budget",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Description"
-              name="projectDesc"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input the Project's Descriptions",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-          </Form>
-        </Modal>
+              <Form.Item
+                label="Budget"
+                name="projectBudget"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input the Project's Budget",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Description"
+                name="projectDesc"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input the Project's Descriptions",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Form>
+          </Modal>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 
   //Example: command to use to re-route on button click
