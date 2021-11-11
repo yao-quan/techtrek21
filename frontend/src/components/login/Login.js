5e13e1509dd97adb2dc95496273f4580c624c89a
ktan18879 <ktan18879@gmail.com>
2021-11-11 11:36:55 +0800

    Update to Login

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,5 +1,22 @@
-function Login() {
-  return <h1>Landing - Login</h1>;
+function Login({ username, password }) {
+  return (
+    <div className="login-wrapper">
+    <h1>Landing Page - Login</h1>
+    <form>
+        <label>
+            <p>Username</p>
+                <input type="text" onChange={e => setUserName(e.target.value)}/>
+            </label>
+        <label>
+            <p>Password</p>
+            <input type="password" onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+    </div>
+  )
   //Example: command to use to re-route on button click
   //window.location.replace("/Login");
 }
