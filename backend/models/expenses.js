ccc6689ca0932301e02494e91acb4bdc0c133a52
lycena <lycena@outlook.com>
2021-11-11 15:02:37 +0800

    remove updated_at and created_at
    handled by timestamp

--- backend/models/expenses.js
+++ backend/models/expenses.js
@@ -27,18 +27,12 @@ const ExpenseSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-    created_at: {
-      type: String,
-      required: true,
-    },
+
     created_by: {
       type: String,
       required: true,
     },
-    updated_at: {
-      type: String,
-      required: true,
-    },
+
     updated_by: {
       type: String,
       required: true,
