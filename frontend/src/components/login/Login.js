ac9b8800a30c45f248dbf898ede6ace49c1c64a9
ktan18879 <ktan18879@gmail.com>
2021-11-11 12:18:39 +0800

    Login Card

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,20 +1,30 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 function Login({ username, password }) {
   return (
     <div className="login-wrapper">
-    <h1>Landing Page - Login</h1>
-    <form>
-        <label>
-            <p>Username</p>
-                <input type="text" onChange={e => setUserName(e.target.value)}/>
-            </label>
-        <label>
-            <p>Password</p>
-            <input type="password" onChange={e => setPassword(e.target.value)}/>
-        </label>
-        <div>
-            <button type="submit">Submit</button>
-        </div>
-    </form>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <form>
+              <label>
+                  <p>Username</p>
+                      <input type="text" onChange={e => username = (e.target.value)}/>
+                  </label>
+              <label>
+                  <p>Password</p>
+                  <input type="password" onChange={e => password = (e.target.value)}/>
+              </label>
+              <div>
+                  <button type="submit">Submit</button>
+              </div>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   )
   //Example: command to use to re-route on button click
